7af858686c00310f2aff830fee6b57cb90cde6ba
BenJohannson <57221492+BenJohannson@users.noreply.github.com>
2022-06-05 13:14:18 -0400

    js: created subcomponents for Alarm

--- src/components/Alarm/index.js
+++ src/components/Alarm/index.js
@@ -1,8 +1,39 @@
-
+import {
+    AlarmMain,
+    InnerContainer,
+    ControlsContainer,
+    Input,
+    Select,
+    Option,
+    Button,
+    DisplayAlarmContainer,
+} from "./styles/alarmStyles"
 
 
 const Alarm = ({children, ...props}) => {
     return ( <AlarmMain {...props}>{children}</AlarmMain> );
+}
+
+Alarm.InnerContainer = function AlarmInnerContainer({children, ...props}) {
+    <InnerContainer {...props}>{children}</InnerContainer>
+}
+Alarm.ControlsContainer = function AlarmControlsContainer({children, ...props}) {
+    <ControlsContainer {...props}>{children}</ControlsContainer>
+}
+Alarm.Input = function AlarmInput({children, ...props}) {
+    <Input {...props}>{children}</Input>
+}
+Alarm.Select = function AlarmSelect({children, ...props}) {
+    <Select {...props}>{children}</Select>
+}
+Alarm.Option = function AlarmOption({children, ...props}) {
+    <Option {...props}>{children}</Option>
+}
+Alarm.Button = function AlarmButton({children, ...props}) {
+    <Button {...props}>{children}</Button>
+}
+Alarm.DisplayAlarmContainer = function AlarmDisplayAlarmContainer({children, ...props}) {
+    <DisplayAlarmContainer {...props}>{children}</DisplayAlarmContainer>
 }
  
 export default Alarm;
---
+++ src/components/Alarm/styles/alarmStyles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const AlarmMain = styled.div``
+export const InnerContainer = styled.div``
+export const ControlsContainer = styled.div``
+export const Input = styled.input``
+export const Select = styled.select``
+export const Option = styled.option``
+export const Button = styled.button``
+export const DisplayAlarmContainer = styled.div``
