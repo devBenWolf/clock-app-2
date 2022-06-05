3305a0067c01eeaab7c3332545fcb5b8792371e7
BenJohannson <57221492+BenJohannson@users.noreply.github.com>
2022-06-05 13:05:33 -0400

    js: changed operators for messages

--- src/containers/ClockContainer.js
+++ src/containers/ClockContainer.js
@@ -56,9 +56,9 @@ const ClockContainer = () => {
         // set greeting based on time of day
         if (convertedHour >= 6 && convertedHour <= 12) {
             setGreeting("Good Morning, it's currently...")
-        } else if (convertedHour > 12 && convertedHour <= 17) {
+        } else if (convertedHour >= 12 && convertedHour <= 17) {
             setGreeting("Good Afternoon, it's currently...")
-        } else if (convertedHour > 17 && convertedHour <= 18)  {
+        } else if (convertedHour > 17 && convertedHour <= 20)  {
             setGreeting("Good Evening, it's currently...")
         } else {
             setGreeting("Good Night, it's currently...")
