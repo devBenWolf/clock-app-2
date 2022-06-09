73ce5334d74eadc7889c58387c181d63451ea7f6
BenJohannson <57221492+BenJohannson@users.noreply.github.com>
2022-06-09 18:33:55 -0400

    js:
    added open, setOpen state boolean,
    
    added Audio constructor
    
    added checkAlarm functon
    
    added setAlarm function
    
    added clearAlarm function

--- src/containers/AlarmContainer.js
+++ src/containers/AlarmContainer.js
@@ -10,10 +10,51 @@ const AlarmContainer = (props) => {
         alarmMinutes: "",
         alarmMeridian: "AM",
         alarmTime: "",
-        currentTime : props.amendedTimeOfDay
     }
 
+
     const [values, setValues] = useState(initialValues)
+    const [open, setOpen] = useState(false)
+
+    // useMemo prevents alarm duplication
+    const audio = useMemo(() => new Audio("./alarm.ogg"), [])
+    audio.loop = true
+    audio.playbackRate = 0.8
+
+    const checkAlarm = () => {
+        if (localStorage.getItem("alarm") === props.amendedTimeOfDay) {
+          audio.play()
+
+        }
+      }
+      checkAlarm()
+
+    useEffect(() => {
+        checkAlarm()
+    }, [])
+
+    const setAlarm = () => {
+        const alarmHoursArray = values.alarmHours.split("")
+        const alarmMinutesArray = values.alarmMinutes.split("")
+        if (isNaN(values.alarmHours)) {
+          alert("alarm must be composed of number and entered in HH:MM format")
+        }
+        
+        if (alarmHoursArray.length < 2 || alarmMinutesArray.length < 2) {
+          alert("you must set alarm in HH:MM format")
+          setValues({...values, alarmHours: "", alarmMinutes: ""})
+          setValues({...values, alarmTime: ""})
+        } else {
+          setValues({...values, alarmTime: `${values.alarmHours}:${values.alarmMinutes} ${values.alarmMeridian}`})
+          localStorage.setItem("alarm", `${values.alarmHours}:${values.alarmMinutes} ${values.alarmMeridian}`)
+        }
+      }
+
+      const clearAlarm = () => {
+        setValues({...values, alarmHours: "", alarmMinutes: "", alarmMeridian: "AM", alarmTime: ""})
+        localStorage.removeItem("alarm")
+        audio.pause()
+      }  
 
     const handleInputs = (event) => {
         const {name, value} = event.target
@@ -24,17 +65,10 @@ const AlarmContainer = (props) => {
         })
     }
 
-    const getTime = () => {
-        const time = new Intl.DateTimeFormat("en", {hour: `2-digit`, minute: `2-digit`})
-        let formattedTime = time.format()
-
-        setValues({...values, currentTime: formattedTime})
-    }
-    console.log(values.alarmHours)
-
     return ( 
         <AlarmMain>
             <Alarm.InnerContainer>
+                <FaAngleDoubleDown size={40} />
                 <Alarm.ControlsContainer>
                     <Alarm.InputContainer>
                         <Alarm.Input 
@@ -42,6 +76,7 @@ const AlarmContainer = (props) => {
                             name = "alarmHours"
                             value = {values.alarmHours}
                             placeholder = "HH"
+                            min="1"
                             onChange = {handleInputs}
                         />
     
@@ -50,6 +85,7 @@ const AlarmContainer = (props) => {
                             name = "alarmMinutes"
                             value = {values.alarmMinutes}
                             placeholder = "MM"
+                            min="1"
                             onChange = {handleInputs}
                         />
     
@@ -64,9 +100,8 @@ const AlarmContainer = (props) => {
                     </Alarm.InputContainer>
 
                     <Alarm.ButtonContainer>
-                        <Alarm.Button>set alarm</Alarm.Button>
-                        <Alarm.Button>stop alarm</Alarm.Button>
-                        <Alarm.Button>clear alarm</Alarm.Button>
+                        <Alarm.Button onClick = {setAlarm}>set alarm</Alarm.Button>
+                        <Alarm.Button onClick = {clearAlarm}>clear alarm</Alarm.Button>
                     </Alarm.ButtonContainer>
                     <Alarm.DisplayAlarmContainer>
 
