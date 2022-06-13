f0707698a25e634fa6cdf5e4c4eb7e41401908b4
BenJohannson <57221492+BenJohannson@users.noreply.github.com>
2022-06-13 11:35:17 -0400

    Update ClockContainer.js
    removed unnecessary commented code

--- src/containers/ClockContainer.js
+++ src/containers/ClockContainer.js
@@ -46,57 +46,6 @@ const ClockContainer = () => {
         }
 
         setTimeOfDay(formattedTime)
-        // separate hours, minutes, meridian modifier
-        //const [time, modifier] = formattedTime.split(" ")
-        //let [hour, minute] = formattedTime.split(":")
-        //setMeridian(modifier)
-
-        // change 12hour to 24hour
-        //if (hour === "12") {
-        //    hour = "00"
-        //}
-//
-        //if (modifier === "PM") {
-        //    hour = parseInt(hour, 10) + 12
-        //}
-//
-        //let convertedTime = `${hour}:${minute}`
-        //let noModifier = convertedTime.slice(0, -2)
-        //setTimeOfDay(noModifier)
-        //setAltTimeOfDay(noModifier)
-//
-        //// isolate hours 
-        //let convertedHour = convertedTime.slice(0, -6)
-//
-        //// use hours to determine day or night background
-        //if (convertedHour > 6 && convertedHour <= 18 ) {
-        //    setBackground(true)
-        //} else {
-        //    setBackground(false)
-        //}
-//
-        //console.log(hours)
-        // set greeting based on time of day
-        //if (convertedHour >= 6 && convertedHour <= 12) {
-        //    setGreeting("Good Morning, it's currently...")
-        //} else if (convertedHour >= 12 && convertedHour <= 17) {
-        //    setGreeting("Good Afternoon, it's currently...")
-        //} else if (convertedHour > 17 && convertedHour <= 20)  {
-        //    setGreeting("Good Evening, it's currently...")
-        //} else {
-        //    setGreeting("Good Night, it's currently...")
-        //}
-
-
-        //if (hours >= 6 && hours <= 12) {
-        //    setGreeting("Good Morning, it's currently...")
-        //} else if (hours >= 12 && hours <= 17) {
-        //    setGreeting("Good Afternoon, it's currently...")
-        //} else if (hours > 17 && hours <= 20)  {
-        //    setGreeting("Good Evening, it's currently...")
-        //} else {
-        //    setGreeting("Good Night, it's currently...")
-        //}
     }
     // get geolocation data
     const getLocationData = async () => {
