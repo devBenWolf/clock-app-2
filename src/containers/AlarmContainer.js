b03650d4e004d8bbf1d8be5ab3d90a27f37195b7
BenJohannson <57221492+BenJohannson@users.noreply.github.com>
2022-06-13 08:59:05 -0400

    Update AlarmContainer.js
    js: changed react-icons for alarm button
        component
    
        removed buttonClick state for open
        state

--- src/containers/AlarmContainer.js
+++ src/containers/AlarmContainer.js
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import Alarm from "../components/Alarm";
 import { AlarmMain } from "../components/Alarm/styles/alarmStyles";
 import Clock from "../components/Clock";
-import { FaAngleDoubleDown, FaAngleDoubleUp } from "react-icons/fa";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 
 
 const AlarmContainer = (props) => {
@@ -71,10 +71,10 @@ const AlarmContainer = (props) => {
             <Alarm.InnerContainer>
                             <Clock.ButtonArticle>
                 <Clock.Button
-                    onClick={() => props.setButtonClick(!buttonClick)}
-                >{ buttonClick ? "Less" : "More"}
+                    onClick={() => setOpen(!open)}
+                >{ open ? "Less" : "More"}
                     <Clock.ButtonSpan>
-                        { buttonClick ? <FaChevronUp style={{color: "white"}} /> : <FaChevronDown style={{color: "white"}} /> }
+                        { open ? <FaChevronUp style={{color: "white"}} /> : <FaChevronDown style={{color: "white"}} /> }
                     </Clock.ButtonSpan>
                 </Clock.Button>
             </Clock.ButtonArticle>
